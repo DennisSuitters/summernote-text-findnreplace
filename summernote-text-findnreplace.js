40d7a8bd50b84ebbd1f6f412ccbcecce0a292ed6
root <dennis@studiojunkyard.com>
2017-12-02 22:58:56 +1100

    Fix replace selected.

--- summernote-text-findnreplace.js
+++ summernote-text-findnreplace.js
@@ -85,7 +85,7 @@
           var fnrCode    = context.invoke('code');
           var fnrFind    = $('.note-findnreplace-find').val();
           var fnrReplace = $('.note-findnreplace-replace').val();
-          var fnrCount   = (fnrCode.match(new RegExp(fnrFind, "g")) || []).length
+          var fnrCount   = (fnrCode.match(new RegExp(fnrFind, "gi")) || []).length
           if (fnrFind) {
             $('#findnreplace-info').text(fnrCount + lang.findnreplace.findResult + "`" + fnrFind + "`");
             var fnrReplaced = fnrCode.replace(new RegExp("(" + fnrFind + ")","gi"),'<u class="note-findnreplace" style="' + options.findnreplace.highlight + '">$1</u>');
@@ -104,18 +104,15 @@
             $('#findnreplace-info').text(fnrCount + lang.findnreplace.findResult + "`" + fnrFind + "`" + lang.findnreplace.replaceResult +"`" + fnrReplace + "`");
             var fnrReplaced = fnrCode.replace(new RegExp(fnrFind,"gi"),fnrReplace);
             $note.summernote('code',fnrReplaced);
-          }else{
+          } else {
             if (fnrReplace) {
               if($note.summernote('createRange').toString()){
-                var fnrSelected = $note.summernote('createRange').toString();
-                var fnrReplaced = fnrCode.replace(new RegExp(fnrSelected,"gi"),fnrReplace);
-                $note.summernote('code',fnrReplaced);
-              }else{
+                $note.summernote('insertText',fnrReplace);
+                $('#findnreplace-info').text('');
+              } else
                 $('#findnreplace-info').html('<span class="text-danger">' + lang.findnreplace.noneSelected + '</span>');
-              }
-            }else{
+            } else
               $('#findnreplace-info').html('<span class="text-danger">' + lang.findnreplace.replaceError + '</span>');
-            }
           }
         });
       };
@@ -125,3 +122,54 @@
     }
   });
 }));
+function getInputSelection(el) {
+    var start = 0, end = 0, normalizedValue, range,
+        textInputRange, len, endRange;
+
+    if (typeof el.selectionStart == "number" && typeof el.selectionEnd == "number") {
+        start = el.selectionStart;
+        end = el.selectionEnd;
+    } else {
+        range = document.selection.createRange();
+
+        if (range && range.parentElement() == el) {
+            len = el.value.length;
+            normalizedValue = el.value.replace(/\r\n/g, "\n");
+
+            // Create a working TextRange that lives only in the input
+            textInputRange = el.createTextRange();
+            textInputRange.moveToBookmark(range.getBookmark());
+
+            // Check if the start and end of the selection are at the very end
+            // of the input, since moveStart/moveEnd doesn't return what we want
+            // in those cases
+            endRange = el.createTextRange();
+            endRange.collapse(false);
+
+            if (textInputRange.compareEndPoints("StartToEnd", endRange) > -1) {
+                start = end = len;
+            } else {
+                start = -textInputRange.moveStart("character", -len);
+                start += normalizedValue.slice(0, start).split("\n").length - 1;
+
+                if (textInputRange.compareEndPoints("EndToEnd", endRange) > -1) {
+                    end = len;
+                } else {
+                    end = -textInputRange.moveEnd("character", -len);
+                    end += normalizedValue.slice(0, end).split("\n").length - 1;
+                }
+            }
+        }
+    }
+
+    return {
+        start: start,
+        end: end
+    };
+}
+
+function replaceSelectedText(el, text) {
+    var sel = getInputSelection(el), val = el.value;
+    el.value = val.slice(0, sel.start) + text + val.slice(sel.end);
+}
+
